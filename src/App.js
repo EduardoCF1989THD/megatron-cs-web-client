0e4a51d2ca5083f1d92181d6b6a540460d1cec36
Eduardo Castillo Fernandez <eduardo_castillo_fernandez@homedepot.com>
2018-06-06 16:59:17 -0400

    Added a new component for the cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import DirectoryItem from './components/directory-item/DirectoryItem';
+import Cart from './components/cart/Cart';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <DirectoryItem/>
-       
+        <Cart/>
+        <div className="grid-container">
+          <DirectoryItem className="grid-item" name="Iron man" image="images/ironman1.png"/>
+          <DirectoryItem name="Iron man" image="images/ironman1.png"/>
+          <DirectoryItem name="Iron man" image="images/ironman1.png"/>
+          <DirectoryItem name="Iron man" image="images/ironman1.png"/>
+        </div>
       </div>
     );
   }
--- src/components/directory-item/DirectoryItem.js
+++ src/components/directory-item/DirectoryItem.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import './DirectoryItem.css';
 
-export default class DIrectoryItem extends Component{
-    
+export default class DirectoryItem extends Component{
+    constructor(props){
+        super(props);
+    }
+
     render(){
         return (
-            <div>
-                <img className="directory-item-image" src="images/ironman1.png"></img>
-                <h2>Iron man</h2>
+            <div className="directory-grid">
+                <img className="directory-item-image" src={this.props.image}></img>
+                <h2>{this.props.name}</h2>
                 <button>Add to cart</button>
             </div>
         );
