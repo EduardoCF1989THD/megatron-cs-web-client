8679998e5cfde81311b33b28dd9281f8a818956c
Eduardo Castillo Fernandez <eduardo_castillo_fernandez@homedepot.com>
2018-06-07 16:20:50 -0400

    Started working in the Description component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Directory from './containers/directory/Directory';
+import Description from './containers/description/Description';
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <Directory></Directory>
+        <Description/>
       </div>
     );
   }
---
+++ src/containers/description/Description.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Description.css';
+
+
+export default class Description extends Component{
+    render(){
+        return(
+            <div>
+                <img src="images/ironman1.png"/>
+                <button>Add to cart</button>
+                <h2>Ironman</h2>
+                <p>This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                </p>
+            </div>
+        );
+    }
+}
