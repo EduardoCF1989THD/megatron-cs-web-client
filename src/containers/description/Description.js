6d0338419ccc28117c2cf05351afae40a8d5f5fc
Eduardo Castillo Fernandez <eduardo_castillo_fernandez@homedepot.com>
2018-06-08 11:55:57 -0400

    Added the elements for the Description container and positioned them

--- src/containers/description/Description.js
+++ src/containers/description/Description.js
@@ -5,15 +5,19 @@ import './Description.css';
 export default class Description extends Component{
     render(){
         return(
-            <div>
-                <img src="images/ironman1.png"/>
-                <button>Add to cart</button>
-                <h2>Ironman</h2>
-                <p>This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
-                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
-                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
-                This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
-                </p>
+            <div className="description-container">
+                <div className="description-image-container">
+                    <img className="description-image" src="images/ironman1.png"/>
+                    <button className="add-to-cart-button">Add to cart</button>
+                </div>
+                <div className="description-text-container">
+                    <h2>Ironman</h2>
+                    <p>This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                    This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                    This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                    This is the description of the comic. skfsdkjfksjnfksfjkbfakbaskfbkdasf
+                    </p>
+                </div>
             </div>
         );
     }
